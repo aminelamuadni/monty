d53bef1b65afe2d2977c982f9cd0703528e66e82
Amine Lamuadni <W7equwE5@gmail.com>
2023-09-21 13:24:21 -0700

    Update push.c file

--- push.c
+++ push.c
@@ -9,27 +9,23 @@
 void push(stack_t **stack, unsigned int line_number, const char *n_str)
 {
 	stack_t *new_node;
-	int num;
 
-	num = atoi(n_str);
-	if (num == 0 && *n_str != '0')
-	{
-		fprintf(stderr, "L%u: usage: push integer\n", line_number);
-		exit(EXIT_FAILURE);
-	}
+	(void)line_number;
 
 	new_node = malloc(sizeof(stack_t));
+
 	if (!new_node)
 	{
 		fprintf(stderr, "Error: malloc failed\n");
 		exit(EXIT_FAILURE);
 	}
 
-	new_node->n = num;
+	new_node->n = atoi(n_str);
 	new_node->prev = NULL;
 	new_node->next = *stack;
 
 	if (*stack)
 		(*stack)->prev = new_node;
+
 	*stack = new_node;
 }
