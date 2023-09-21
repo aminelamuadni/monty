1c74a3cca29fd56148e64fe06983092f98749c7d
Amine Lamuadni <W7equwE5@gmail.com>
2023-09-21 15:08:35 -0700

    Update monty.c file

--- monty.c
+++ monty.c
@@ -1,79 +1,6 @@
 #include "monty.h"
 
 /**
- * is_digit - Check if a given string is a valid integer.
- * @str: String to be checked.
- *
- * Return: 1 if string is a valid integer, otherwise 0.
- */
-int is_digit(const char *str)
-{
-	if (!str)
-		return (0);
-
-	if (*str == '-' || *str == '+')
-		str++;
-
-	while (*str)
-	{
-		if (!isdigit(*str))
-			return (0);
-		str++;
-	}
-	return (1);
-}
-
-/**
- * free_stack - Frees the memory used by a doubly linked stack.
- * @stack: Head of the stack.
- *
- * Description: Iterates through each element of the stack
- * and frees the memory used by each node.
- */
-void free_stack(stack_t *stack)
-{
-	stack_t *temp;
-
-	while (stack)
-	{
-		temp = stack;
-		stack = stack->next;
-		free(temp);
-	}
-}
-
-/**
- * execute_line - Processes a line of Monty bytecode.
- * @line: The line from the Monty bytecode file.
- * @stack: Double pointer to the beginning of the stack.
- * @line_number: The line number of the current instruction.
- *
- * Return: 0 if the line is processed without error, otherwise 1.
- */
-int execute_line(char *line, stack_t **stack, unsigned int line_number)
-{
-	char *opcode, *argument;
-
-	opcode = strtok(line, " \t\n");
-	if (!opcode || opcode[0] == '#')
-		return (0);
-
-	argument = strtok(NULL, " \t\n");
-
-	if (strcmp(opcode, "push") == 0)
-	{
-		if (push(stack, line_number, argument) == 1)
-			return (1);
-	}
-	else if (strcmp(opcode, "pall") == 0)
-	{
-		pall(stack, line_number);
-	}
-
-	return (0);
-}
-
-/**
  * main - The primary function for the Monty interpreter.
  * @argc: Number of arguments passed to the program.
  * @argv: Array of arguments.
@@ -103,7 +30,13 @@ int main(int argc, char **argv)
 	while (fgets(line, sizeof(line), file))
 	{
 		line_number++;
-		execute_line(line, &stack, line_number);
+
+		if (execute_line(line, &stack, line_number) != 0)
+		{
+			free_stack(stack);
+			fclose(file);
+			exit(EXIT_FAILURE);
+		}
 	}
 
 	free_stack(stack);
