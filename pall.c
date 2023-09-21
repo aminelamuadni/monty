229f2ac48665d46f278b13e1ca113e1b9351649c
Amine Lamuadni <W7equwE5@gmail.com>
2023-09-21 13:57:58 -0700

    Update pall.c file

--- pall.c
+++ pall.c
@@ -1,19 +1,19 @@
 #include "monty.h"
 
 /**
- * pall - Prints all the values on the stack, starting from the top.
- * @stack: Double pointer to the head of the stack.
- * @line_number: Line number of the instruction in the Monty bytecode file.
+ * pall - Prints all values on the stack, starting from the top.
+ * @stack: A double pointer to the head of the stack.
+ * @line_number: The line number of the current opcode.
  */
 void pall(stack_t **stack, unsigned int line_number)
 {
-	stack_t *current = *stack;
+	stack_t *temp = *stack;
 
 	(void)line_number;
 
-	while (current)
+	while (temp)
 	{
-		printf("%d\n", current->n);
-		current = current->next;
+		printf("%d\n", temp->n);
+		temp = temp->next;
 	}
 }
