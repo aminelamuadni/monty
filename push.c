4ac82f9eca3aedd9cdaf9d95377f14d756f69576
Amine Lamuadni <W7equwE5@gmail.com>
2023-09-21 13:57:58 -0700

    Update push.c file

--- push.c
+++ push.c
@@ -1,31 +1,36 @@
 #include "monty.h"
 
 /**
- * push - Pushes an element to the stack.
- * @stack: Double pointer to the head of the stack.
- * @line_number: Line number of the instruction in the Monty bytecode file.
+ * push - Pushes an integer onto the top of the stack.
+ * @stack: A double pointer to the head of the stack.
+ * @line_number: The line number of the current opcode.
  * @n_str: The string representation of the integer to be pushed.
+ *
+ * Return: 0 on success, 1 on error.
  */
-void push(stack_t **stack, unsigned int line_number, const char *n_str)
+int push(stack_t **stack, unsigned int line_number, const char *n_str)
 {
 	stack_t *new_node;
 
-	(void)line_number;
+	if (!n_str || !is_digit(n_str))
+	{
+		fprintf(stderr, "L%d: usage: push integer\n", line_number);
+		return (1);
+	}
 
 	new_node = malloc(sizeof(stack_t));
-
 	if (!new_node)
 	{
 		fprintf(stderr, "Error: malloc failed\n");
-		exit(EXIT_FAILURE);
+		return (1);
 	}
 
 	new_node->n = atoi(n_str);
-	new_node->prev = NULL;
 	new_node->next = *stack;
-
+	new_node->prev = NULL;
 	if (*stack)
 		(*stack)->prev = new_node;
-
 	*stack = new_node;
+
+	return (0);
 }
