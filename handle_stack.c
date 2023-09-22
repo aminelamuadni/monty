1f626ff0654882afebc50c1c72f3ea8d1e780176
Amine Lamuadni <W7equwE5@gmail.com>
2023-09-22 14:26:37 -0700

    Add handle_stack.c file

---
+++ handle_stack.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ * handle_stack - Sets the mode to stack.
+ * @stack: A double pointer to the beginning of the stack.
+ * @line_number: The line number of the current instruction.
+ */
+void handle_stack(stack_t **stack, unsigned int line_number)
+{
+	(void) stack;
+	(void) line_number;
+
+	mode = 0;
+}
