4256867280a7b29ecb0520fcaabc552d0f20b101
Amine Lamuadni <W7equwE5@gmail.com>
2023-09-22 07:21:55 -0700

    Add handle_nop.c file

---
+++ handle_nop.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ * handle_nop - This function does nothing.
+ * @stack: Double pointer to the head of the Stack.
+ * @line_number: Represents the line number.
+ *
+ * Description: This function is designed to handle the nop operation.
+ */
+void handle_nop(stack_t **stack, unsigned int line_number)
+{
+	(void) stack;
+	(void) line_number;
+}
