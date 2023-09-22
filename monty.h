2159543f025b4534fc8d1f1dfa06019569338c7f
Amine Lamuadni <W7equwE5@gmail.com>
2023-09-22 14:05:57 -0700

    Update monty.h file

--- monty.h
+++ monty.h
@@ -52,5 +52,6 @@ void handle_div(stack_t **stack, unsigned int line_number);
 void handle_mul(stack_t **stack, unsigned int line_number);
 void handle_mod(stack_t **stack, unsigned int line_number);
 void handle_pchar(stack_t **stack, unsigned int line_number);
+void handle_pstr(stack_t **stack, unsigned int line_number);
 
 #endif /* MONTY_H */
